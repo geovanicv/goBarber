2a595519b836170a0a81561c6dbef530cdb3b019
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-01-25 18:07:20 -0300

    Configurando banco de dados postgres

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'zsxx4755',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
