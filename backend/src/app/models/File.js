a6e0c9b788413ea16b6af478716bc3c2ddff904b
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-01-28 03:50:10 -0300

    Enviando avatar do usuario

---
+++ backend/src/app/models/File.js
@@ -0,0 +1,19 @@
+import Sequelize, { Model } from 'sequelize';
+
+class File extends Model {
+  static init(connection) {
+    super.init(
+      {
+        name: Sequelize.STRING(),
+        path: Sequelize.STRING(),
+      },
+      {
+        sequelize: connection,
+      }
+    );
+
+    return this;
+  }
+}
+
+export default File;
