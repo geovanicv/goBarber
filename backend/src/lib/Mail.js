85ce7fe6313a686b7f98f7bd038213b1eb960e63
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-01-30 21:19:08 -0300

    Configurando templates de email

--- backend/src/lib/Mail.js
+++ backend/src/lib/Mail.js
@@ -1,4 +1,8 @@
 import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+
 import mailConfig from '../config/mail';
 
 class Mail {
@@ -12,6 +16,26 @@ class Mail {
       // algumas estratégias de envio de email nao possui autenticação de usuário
       // entao se nao tiver usuário passa nulo
     });
+
+    this.configureTemplates();
+  }
+
+  // configuração dos templates de email
+  configureTemplates() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
   }
 
   sendMail(message) {
