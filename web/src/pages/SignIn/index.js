54f94475c9b6b586a93aa818dd8009a3e247e470
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-02-06 03:33:38 -0300

    Desenvolvendo sign in e sign out page

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="goBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder=" Sua senha secreta"
+        />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
 }
