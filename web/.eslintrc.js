9f44a9a87fa81d702aa7673444ee42db06728e33
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-02-19 00:44:38 -0300

    Alterações...

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -41,11 +41,5 @@ module.exports = {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn"
   },
-  settings: {
-    "import/resolver": {
-      "babel-plugin-root-import": {
-        rootPathSuffix: "src"
-      }
-    }
-  }
+
 };
