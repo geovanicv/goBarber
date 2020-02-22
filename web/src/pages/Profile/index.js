8540b652cbdadaf27c287ce8bc8f18a41a120131
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-02-21 22:07:40 -0300

    Atualizando perfil

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Form, Input } from '@rocketseat/unform';
+import { updateProfileRequest } from '../../store/modules/user/actions';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
@@ -21,8 +25,8 @@ export default function Profile() {
         <Input type="password" name="oldPassword" placeholder="Senha atual" />
         <Input type="password" name="password" placeholder="Nova senha" />
         <Input
-          type="confirmPassword"
-          name="password"
+          type="password"
+          name="confirmPassword"
           placeholder="Confirmação de senha"
         />
 
