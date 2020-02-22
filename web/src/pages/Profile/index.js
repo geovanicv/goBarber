8101718c05ceb743af67a992b69ed5f10b218b97
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-02-21 21:33:03 -0300

    Criando página de perfil

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Senha atual" />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="confirmPassword"
+          name="password"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
