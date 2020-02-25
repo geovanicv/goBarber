ce4b10a4af25473c972e92152561a7fecbcf9e21
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-02-25 15:58:30 -0300

    Corrigido erro foto de perfil (versao do unform)

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,35 +1,31 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-import { Form, Input } from '@rocketseat/unform';
-import { updateProfileRequest } from '../../store/modules/user/actions';
+import { Form } from '@unform/web';
 
 import AvatarInput from './AvatarInput';
 
 import { Container } from './styles';
 
 export default function Profile() {
-  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit(data) {
-    dispatch(updateProfileRequest(data));
-  }
+  function handleSubmit(data) {}
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
         <AvatarInput name="avatar_id" />
-        <Input name="name" placeholder="Nome completo" />
-        <Input name="email" placeholder="Seu endereço de e-mail" />
+        <input name="name" placeholder="Nome completo" />
+        <input name="email" placeholder="Seu endereço de e-mail" />
 
         <hr />
 
-        <Input type="password" name="oldPassword" placeholder="Senha atual" />
-        <Input type="password" name="password" placeholder="Nova senha" />
-        <Input
-          type="password"
-          name="confirmPassword"
+        <input type="password" name="oldPassword" placeholder="Senha atual" />
+        <input type="password" name="password" placeholder="Nova senha" />
+        <input
+          type="confirmPassword"
+          name="password"
           placeholder="Confirmação de senha"
         />
 
