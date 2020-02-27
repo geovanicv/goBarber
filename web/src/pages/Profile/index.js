71d45c939f5e70bd4ca14677422cc685de4db027
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-02-26 21:08:51 -0300

    Corrigido todos os problemas das funcionalidades de perfil do usuário (edição, input unform)

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,31 +1,41 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { Form } from '@unform/web';
+import Input from './Input';
+
+import { updateProfileRequest } from '../../store/modules/user/actions';
 
 import AvatarInput from './AvatarInput';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const formRef = useRef();
+
+  const dispatch = useDispatch();
+
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    console.tron.log(data);
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
-      <Form initialData={profile} onSubmit={handleSubmit}>
+      <Form ref={formRef} initialData={profile} onSubmit={handleSubmit}>
         <AvatarInput name="avatar_id" />
-        <input name="name" placeholder="Nome completo" />
-        <input name="email" placeholder="Seu endereço de e-mail" />
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Seu endereço de e-mail" />
 
         <hr />
 
-        <input type="password" name="oldPassword" placeholder="Senha atual" />
-        <input type="password" name="password" placeholder="Nova senha" />
-        <input
-          type="confirmPassword"
-          name="password"
+        <Input type="password" name="oldPassword" placeholder="Senha atual" />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
           placeholder="Confirmação de senha"
         />
 
