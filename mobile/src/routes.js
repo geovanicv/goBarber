ce3431003c2f49014e0dd05467cbb4b0be3348ec
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-03-10 02:55:03 -0300

    Configurada as rotas de agendar

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {TouchableOpacity} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {useSelector} from 'react-redux';
@@ -10,8 +11,57 @@ import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
 
-const Stack = createStackNavigator();
+import Confirm from './pages/New/Confirm';
+import SelectDateTime from './pages/New/SelectDateTime';
+import SelectProvider from './pages/New/SelectProvider';
+
+const StackLogin = createStackNavigator();
+const StackAgendamento = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const StackLoginScreen = () => (
+  <StackLogin.Navigator headerMode="none">
+    <StackLogin.Screen name="SignIn" component={SignIn} />
+    <StackLogin.Screen name="SignUp" component={SignUp} />
+  </StackLogin.Navigator>
+);
+
+const StackAgendamentoScreen = ({navigation}) => (
+  <StackAgendamento.Navigator
+    screenOptions={{
+      headerTintColor: '#FFF',
+      headerTransparent: true,
+      headerLeftContainerStyle: {
+        marginLeft: 20,
+      },
+    }}>
+    <StackAgendamento.Screen
+      name="SelectProvider"
+      component={SelectProvider}
+      options={{
+        title: 'Selecione o prestador',
+        headerLeft: () => (
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate('Dashboard');
+            }}>
+            <Icon name="chevron-left" size={20} color="#FFF" />
+          </TouchableOpacity>
+        ),
+      }}
+    />
+    <StackAgendamento.Screen
+      name="SelectDateTime"
+      component={SelectDateTime}
+      options={{title: 'Selecione a data'}}
+    />
+    <StackAgendamento.Screen
+      name="Confirm"
+      component={Confirm}
+      options={{title: 'Confirmar'}}
+    />
+  </StackAgendamento.Navigator>
+);
 
 function Routes() {
   const signed = useSelector(state => state.auth.signed);
@@ -34,6 +84,8 @@ function Routes() {
             iconName = 'event';
           } else if (route.name === 'Profile') {
             iconName = 'person';
+          } else if (route.name === 'New') {
+            iconName = 'add-circle-outline';
           }
 
           return <Icon name={iconName} size={20} color="#FFF" />;
@@ -44,6 +96,13 @@ function Routes() {
         component={Dashboard}
         options={{title: 'Agendamentos'}}
       />
+
+      <Tab.Screen
+        name="New"
+        component={StackAgendamentoScreen}
+        options={{title: 'Agendar', tabBarVisible: false}}
+      />
+
       <Tab.Screen
         name="Profile"
         component={Profile}
@@ -51,10 +110,7 @@ function Routes() {
       />
     </Tab.Navigator>
   ) : (
-    <Stack.Navigator headerMode="none">
-      <Stack.Screen name="SignIn" component={SignIn} />
-      <Stack.Screen name="SignUp" component={SignUp} />
-    </Stack.Navigator>
+    <StackLoginScreen />
   );
 }
 
